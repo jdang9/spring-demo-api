ad405afdabd9e1b3ae72f4136e8e87b68ce1f959
JDang <jdang@MBPr0.local>
2023-10-21 18:47:59 -0400

    init repo

---
+++ src/main/java/com/spring/demo/api/springdemoapi/SpringDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.demo.api.springdemoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/demo/api/springdemoapi/SpringDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.api.springdemoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
